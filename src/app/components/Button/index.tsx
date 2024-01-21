3c5fb01d0a3f969d44abc9bf3d4a18b34fb667b9
yuliiatkachenko <ecspetra@gmail.com>
2024-01-20 18:58:10 -0800

    CV section

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -3,7 +3,6 @@ import classNames from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { faCircleArrowDown } from '@fortawesome/free-solid-svg-icons'
-import yuliiaTkachenkoCV from '@/app/assets/cv/CV_Yuliia_Tkachenko_Front-End-Developer.pdf'
 
 type PropsType = {
 	link?: string
@@ -14,7 +13,7 @@ type PropsType = {
 
 const Button: FC<PropsType> = ({ link, icon, children, context = 'basic' }) => {
 	const basicButtonClassNames =
-		'bg-lime-400 text-zinc-950 px-6 py-4 border border-lime-400 rounded-[32px] hover:text-lime-400 after:duration-300 after:w-0 after:h-0 after:bg-zinc-900 after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full hover:after:w-full hover:after:h-full'
+		'bg-lime-400 text-zinc-950 w-fit flex justify-center items-center px-6 py-4 border border-lime-400 rounded-[32px] hover:text-lime-400 after:duration-300 after:w-0 after:h-0 after:bg-zinc-900 after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full hover:after:w-full hover:after:h-full'
 	const simpleButtonClassNames =
 		'bg-zinc-800 border border-zinc-700 px-6 py-4 rounded-[32px] hover:text-zinc-950 hover:border-white after:duration-300 after:w-0 after:h-0 after:bg-white after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full hover:after:w-full hover:after:h-full'
 	const socialLinkButtonClassNames =
@@ -23,9 +22,9 @@ const Button: FC<PropsType> = ({ link, icon, children, context = 'basic' }) => {
 	const isBasicButton = context === 'basic'
 	const buttonText = isBasicButton ? 'Download CV' : 'Preview'
 	const buttonIcon = isBasicButton ? faCircleArrowDown : icon
-	const buttonDownlodLink = isBasicButton
-		? 'CV_Yuliia_Tkachenko_Front-End-Developer.pdf'
-		: undefined
+	const yuliiaTkachenkoCVLink =
+		'/cv/CV_Yuliia_Tkachenko_Front-End-Developer.pdf'
+	const buttonDownloadLink = isBasicButton ? yuliiaTkachenkoCVLink : undefined
 
 	const getButtonClassNames = () => {
 		switch (context) {
@@ -35,44 +34,42 @@ const Button: FC<PropsType> = ({ link, icon, children, context = 'basic' }) => {
 				return socialLinkButtonClassNames
 		}
 	}
-	console.log(yuliiaTkachenkoCV)
+
 	return context === 'basic' || context === 'preview-link' ? (
-		<button
+		<a
+			href={isBasicButton ? yuliiaTkachenkoCVLink : link}
+			target='_blank'
+			rel='noopener noreferrer'
+			download={buttonDownloadLink}
 			className={classNames(
 				'duration-300 text-lg relative font-bold',
 				basicButtonClassNames
 			)}
 		>
-			{/*<a*/}
-			{/*	href={isBasicButton ? yuliiaTkachenkoCV.src : link}*/}
-			{/*	target='_blank'*/}
-			{/*	rel='noopener noreferrer'*/}
-			{/*	download={buttonDownlodLink}*/}
-			{/*>*/}
-			{/*	<span className='relative z-20'>{buttonText}</span>*/}
-			{/*	<FontAwesomeIcon*/}
-			{/*		className='ml-2 relative z-20'*/}
-			{/*		icon={buttonIcon}*/}
-			{/*	/>*/}
-			{/*</a>*/}
-		</button>
+			<span className='relative z-20'>{buttonText}</span>
+			<FontAwesomeIcon
+				className='ml-2 relative z-20'
+				icon={buttonIcon!}
+			/>
+		</a>
 	) : (
-		<button
+		<a
+			href={link}
+			target='_blank'
+			rel='noopener noreferrer'
 			className={classNames(
 				'duration-300 text-lg relative z-50 font-bold',
 				getButtonClassNames()
 			)}
 		>
-			<a href={link} target='_blank' rel='noopener noreferrer'>
-				<span className='relative z-20'>{children}</span>
-				{icon && (
-					<FontAwesomeIcon
-						className={`${children && 'ml-2'} relative z-20`}
-						icon={icon}
-					/>
-				)}
-			</a>
-		</button>
+			<span className='relative z-20'>{children}</span>
+			{icon && (
+				<FontAwesomeIcon
+					className={`${children && 'ml-2'} relative z-20`}
+					icon={icon}
+				/>
+			)}
+		</a>
 	)
 }
 
