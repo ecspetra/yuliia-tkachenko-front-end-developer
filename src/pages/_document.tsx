38cea09086e67be955782157ccb36b54077d0ca8
yuliiatkachenko <ecspetra@gmail.com>
2024-02-08 15:55:27 -0800

    Fix for favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,14 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
 	return (
 		<Html lang='en'>
-			<Head>
-				<title>Yuliia Tkachenko | Front End Developer</title>
-				<link
-					rel='icon'
-					type='image/x-icon'
-					href='/public/assets/images/favicon.ico'
-				/>
-			</Head>
+			<Head />
 			<body>
 				<Main />
 				<NextScript />
