f2bb95d19804e481d23b0f0d4dd05fc7ef8b9ffb
yuliiatkachenko <ecspetra@gmail.com>
2024-02-08 11:57:40 -0800

    Fix for fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
 	return (
 		<Html lang='en'>
-			<Head />
+			<Head>
+				<title>Yuliia Tkachenko | Front End Developer</title>
+				<link
+					rel='icon'
+					type='image/x-icon'
+					href='/public/assets/images/favicon.ico'
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
