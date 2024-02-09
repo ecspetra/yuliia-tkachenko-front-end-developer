7418bd94730c1b34a879ceb2d5580f033d6a9345
yuliiatkachenko <ecspetra@gmail.com>
2024-02-08 17:26:15 -0800

    Fix for favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,12 @@ const Document = () => {
 	return (
 		<Html lang='en'>
 			<Head>
-				<title>HTML Elements Reference</title>
+				<title>Yuliia Tkachenko | Front End Developer</title>
+				<meta
+					name='description'
+					content='Yuliia Tkachenko - Front End Developer with more than 6 years of experience.'
+				/>
+				<meta name='author' content='Yuliia Tkachenko' />
 				<link rel='shortcut icon' href='/icon.ico' />
 			</Head>
 			<body>
