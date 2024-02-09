77b8d80e8aaaa1870d48a4bf0bac015b80f9e031
yuliiatkachenko <ecspetra@gmail.com>
2024-02-08 17:23:31 -0800

    Fix for favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,5 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Metadata } from 'next'
-import Favicon from '../../public/favicon.ico'
-
-export const metadata: Metadata = {
-	title: 'Yuliia Tkachenko | Front End Developer',
-	description:
-		'Yuliia Tkachenko - Front End Developer with more than 6 years of experience.',
-	icons: { icon: Favicon.src },
-	openGraph: {
-		images: Favicon.src,
-	},
-}
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
 	return (
 		<Html lang='en'>
-			<Head />
+			<Head>
+				<title>HTML Elements Reference</title>
+				<link rel='shortcut icon' href='/icon.ico' />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
