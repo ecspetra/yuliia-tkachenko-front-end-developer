375384db57fd0c82ad25792d015e26101ef773fb
yuliiatkachenko <ecspetra@gmail.com>
2024-02-22 12:04:19 -0800

    fixes

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,5 +1,6 @@
 export const LINK_TO_GITHUB_PROFILE = 'https://github.com/ecspetra'
-export const LINK_TO_LINKEDIN_PROFILE = 'www.linkedin.com/in/tkachenko-yuliia-v'
+export const LINK_TO_LINKEDIN_PROFILE =
+	'https://www.linkedin.com/in/tkachenko-yuliia-v'
 export const LINK_TO_TELEGRAM_PROFILE = 'tg://resolve?domain=ecspetra'
 export const LINK_TO_INSTAGRAM_PROFILE =
 	'https://www.instagram.com/tkachenko_yuliia_v'
