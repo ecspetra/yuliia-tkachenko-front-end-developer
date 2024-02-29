e455996ac7421e0f2fb3fc788b1dbb59ab7b1e36
yuliiatkachenko <ecspetra@gmail.com>
2024-02-28 19:22:32 -0800

    Added scale effect for buttons

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -19,13 +19,13 @@ type PropsType = {
 }
 
 const Button: FC<PropsType> = ({ link, icon, children, context = 'basic' }) => {
-	const basicButtonClassNames = `text-zinc-950 w-full lg:w-fit flex justify-center items-center px-6 py-4 rounded-[32px] hover:text-white after:duration-300 after:w-0 after:h-0 after:bg-zinc-900 after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full hover:after:w-[calc(100%-4px)] hover:after:h-[calc(100%-4px)]`
+	const basicButtonClassNames = `text-zinc-950 w-full lg:w-fit flex justify-center items-center px-6 py-4 rounded-[32px] hover:text-white hover:scale-[1.1] after:duration-300 after:w-0 after:h-0 after:bg-zinc-900 after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full hover:after:w-[calc(100%-4px)] hover:after:h-[calc(100%-4px)]`
 	const simpleButtonClassNames =
-		'bg-zinc-800 w-full lg:w-fit flex justify-center items-center border-2 border-zinc-700 px-6 py-4 rounded-[32px] hover:text-zinc-950 hover:border-white after:duration-300 after:w-0 after:h-0 after:bg-white after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full hover:after:w-[calc(100%+2px)] hover:after:h-[calc(100%+2px)]'
+		'bg-zinc-800 w-full lg:w-fit flex justify-center items-center border-2 border-zinc-700 px-6 py-4 rounded-[32px] hover:text-zinc-950 hover:border-white hover:scale-[1.1] after:duration-300 after:w-0 after:h-0 after:bg-white after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full hover:after:w-[calc(100%+2px)] hover:after:h-[calc(100%+2px)]'
 	const socialLinkV1ButtonClassNames =
-		'w-14 h-14 sm:w-16 sm:h-16 bg-zinc-950 !text-2xl flex justify-center items-center rounded-full hover:text-zinc-950'
+		'w-14 h-14 sm:w-16 sm:h-16 bg-zinc-950 !text-2xl flex justify-center items-center rounded-full hover:text-zinc-950 hover:scale-[1.1]'
 	const socialLinkV2ButtonClassNames =
-		'w-14 h-14 sm:w-12 sm:h-12 bg-zinc-800 border-2 border-transparent !text-xl flex justify-center items-center rounded-full hover:text-zinc-950 hover:border-white after:duration-300 after:w-0 after:h-0 after:bg-white after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full hover:after:w-full hover:after:h-full'
+		'w-14 h-14 sm:w-12 sm:h-12 bg-zinc-800 border-2 border-transparent !text-xl flex justify-center items-center rounded-full hover:text-zinc-950 hover:border-white hover:scale-[1.1] after:duration-300 after:w-0 after:h-0 after:bg-white after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full hover:after:w-full hover:after:h-full'
 
 	const isBasicButton = context === 'basic'
 	const buttonText = isBasicButton ? 'Download CV' : 'Preview'
